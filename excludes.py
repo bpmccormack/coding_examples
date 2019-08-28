f74dcbafadd265f0ff7bd7acda0206e5dc64ebca
bpmccormack <54155710+bpmccormack@users.noreply.github.com>
2019-08-28 13:45:02 -0600

    Update excludes.py

--- excludes.py
+++ excludes.py
@@ -7,18 +7,19 @@ Created on Thu Aug 15 14:41:31 2019
 
 import pandas as pd
 
-data = pd.read_excel(r'C:\Users\bryanmccormack\Downloads\asin_list.xlsx')
-df = pd.DataFrame(data, columns=['Track','Asin','Title'])
+data = pd.read_excel(r'C:\Users\bryanmccormack\Downloads\61_NEEDS_REVIEWS_8.27.19.xlsx')
+data = data.astype(str)
+df = pd.DataFrame(data, columns=['Date Added','Track','Asin','Title'])
 
-print (df)
-
-excludes = ["Chainsaw", "Diaper pail", "Leaf Blower"]
+excludes = ['chainsaw','pail','leaf blower','HYOUJIN','brush','dryer','genie','Genuine Joe','backpack',
+                             'curling iron','dog','cat','wig','animal','dryer',':','tea', 'Adidas', 'Fila',
+                             'Reebok','Puma','Nike','basket','extension','extensions','batteries','battery','[EXPLICIT]']
 my_excludes = [set(key_word.lower().split()) for key_word in excludes]
-match_titles = [e for e in df.Title if any(keywords.issubset(e.lower().split()) for keywords in my_excludes)]
+match_titles = [e for e in df.Title if any(keywords.issubset for keywords in my_excludes)]
 
-print(match_titles)
-
-def is_match(title, excludes=["Chainsaw", "Diaper pail", "Leaf Blower"]):
+def is_match(title, excludes = ['chainsaw','pail','leaf blower','HYOUJIN','brush','dryer','genie','Genuine Joe','backpack',
+                             'curling iron','dog','cat','wig','animal','dryer',':','tea', 'Adidas', 'Fila',
+                             'Reebok','Puma','Nike','basket','extension','extensions','batteries','battery','[EXPLICIT]']):
     my_excludes = [set(key_word.lower().split()) for key_word in excludes]
     if any(keywords.issubset(title.lower().split()) for keywords in my_excludes):
         return True
@@ -26,9 +27,9 @@ def is_match(title, excludes=["Chainsaw", "Diaper pail", "Leaf Blower"]):
 df['match_titles'] = df['Title'].apply(is_match)
 result = df[df['match_titles']]['Asin']
 
-print(result)
-
-def is_match(title, excludes=["Chainsaw", "Diaper pail", "Leaf Blower"]):
+def is_match(title, excludes = ['chainsaw','pail','leaf blower','HYOUJIN','brush','dryer','genie','Genuine Joe','backpack',
+                             'curling iron','dog','cat','wig','animal','dryer',':','tea', 'Adidas', 'Fila',
+                             'Reebok','Puma','Nike','basket','extension','extensions','batteries','battery','[EXPLICIT]']):
     my_excludes = [set(key_word.lower().split()) for key_word in excludes]
     if any(keywords.issubset(title.lower().split()) for keywords in my_excludes):
         return True
@@ -36,8 +37,4 @@ def is_match(title, excludes=["Chainsaw", "Diaper pail", "Leaf Blower"]):
 df['match_titles'] = df['Title'].apply(is_match)
 results = df[df['match_titles']]['Track']
 
-print(results)
-
-results.replace('BRYAN','Z-EXCLUDE')
-
-df.to_csv('EXCLUDES.csv',index=False)
+df.to_csv('Asin_List(8.28.19).csv',index=False)
